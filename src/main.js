a02ae17f3db8e9c62d1922b2ea9edb1076bb53f4
Thomas Blumstein <61987113+Oute-Oute@users.noreply.github.com>
2023-02-13 14:07:40 +0100

    link project to firebase app

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import firebaseApp from './plugins/firebase';
+// import { initializeApp } from "firebase/app";
+// import { getDatabase } from "firebase/database";
 
-createApp(App).mount('#app')
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCFirpnYPTh5Izd5ldO8LZa3UtHrLldRak",
+//     authDomain: "securityprojectdi5tb.firebaseapp.com",
+//     databaseURL: "https://securityprojectdi5tb-default-rtdb.europe-west1.firebasedatabase.app",
+//     projectId: "securityprojectdi5tb",
+//     storageBucket: "securityprojectdi5tb.appspot.com",
+//     messagingSenderId: "878316006275",
+//     appId: "1:878316006275:web:106258acce3148665de4b6"
+// };
+
+// const fApp=initializeApp(firebaseConfig);
+// const db=getDatabase();
+//console.log(db)
+
+createApp(App).mount('#app');
---
+++ src/plugins/firebase.js
@@ -0,0 +1,18 @@
+import { getDatabase } from "firebase/database";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCFirpnYPTh5Izd5ldO8LZa3UtHrLldRak",
+    authDomain: "securityprojectdi5tb.firebaseapp.com",
+    databaseURL: "https://securityprojectdi5tb-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "securityprojectdi5tb",
+    storageBucket: "securityprojectdi5tb.appspot.com",
+    messagingSenderId: "878316006275",
+    appId: "1:878316006275:web:106258acce3148665de4b6"
+};
+let firebaseApp;
+firebaseApp=initializeApp(firebaseConfig);
+const db=getDatabase();
+console.log(db)
+
+export default firebaseApp
