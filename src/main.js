81382684c6b4418300ea62490dbb006164f8a836
Thomas Blumstein <61987113+Oute-Oute@users.noreply.github.com>
2023-02-27 16:44:30 +0100

    add bulma

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import firebaseApp from './plugins/firebase';
+import firebaseApp from './firebase';
+require ('@/assets/style/bulma.scss');
+require ('@/assets/style/main.css');
 
 export const app=createApp(App)
 app.use(router)
