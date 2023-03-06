f5c536c5a87a4ea22268c563d08d7e4aa9aab1ca
Thomas Blumstein <61987113+Oute-Oute@users.noreply.github.com>
2023-03-06 12:28:12 +0100

    refactor feed

---
+++ src/components/FeedPage.vue
@@ -0,0 +1,29 @@
+<template>
+    <router-view />
+    <div class="save-btn">
+        <button class="modal-button" @click="showModal = true">New Show</button>
+    </div>
+    <NewShow v-show="showModal" @close-modal="showModal = false" />
+</template>
+
+
+<script>
+import NewShow from './NewShow.vue';
+
+export default {
+    name: 'App',
+    components: {
+        NewShow,
+
+    },
+    data() {
+        return {
+            showModal: false
+        }
+    }
+}
+</script>
+
+<style scoped>
+@import "../assets/style/modal.css"
+</style>
