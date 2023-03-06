7bd3aa9b17251fe33e74c4478c1326a83a0127a3
Thomas Blumstein <61987113+Oute-Oute@users.noreply.github.com>
2023-03-06 12:28:48 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import firebaseApp from './firebase';
-require ('@/assets/style/bulma.scss');
-require ('@/assets/style/main.css');
+require('@/assets/style/bulma.scss');
+require('@/assets/style/main.css');
 
-export const app=createApp(App)
+export const app = createApp(App)
+app.config.globalProperties.AESKey = "oute"
 app.use(router)
 app.mount('#app');
